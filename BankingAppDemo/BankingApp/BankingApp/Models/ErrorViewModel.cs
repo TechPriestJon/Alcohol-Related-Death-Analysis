68d65a1de0f930617c3fa2063dd420da88a41d11
AHumbleDevonian <ahumbledevonian@hotmail.com>
2017-11-22 22:53:26 +0000

    Setup

---
+++ BankingAppDemo/BankingApp/BankingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BankingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
